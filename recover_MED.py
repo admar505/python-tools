a30292a78cb1c7e8e39a164714e4661062e17d46
Diego <diego@veritasgenetics.com>
2017-02-27 15:37:12 -0500

    smol change

--- recover_MED.py
+++ recover_MED.py
@@ -57,7 +57,7 @@ def LoserRecover(ovcf,rsid):
             #else:#neh, put this at the end in case of empty file;
     if success == 0:
         #print ovcf.ALT
-        failreturn = ovcf.CHROM + ":"  + str(ovcf.POS) +  "\t" +  ovcf.CHROM + "\t" + str(ovcf.POS) + "\t" + str(ovcf.REF) + "\t" + str(ovcf.ALT[0]) + "\tFBRefAlleleCount=0\tFBReferenceAlleleQ=" + str(ovcf.QUAL) + "\tEFF_HGVS=OMICIAUNMAPPABLE:" + ovcf.ID + "QUAL=" + str(ovcf.QUAL) + "\t" + "RSID=" + str(ovcf.ID) +  "\n"
+        failreturn = ovcf.CHROM + ":"  + str(ovcf.POS) +  "\t" +  ovcf.CHROM + "\t" + str(ovcf.POS) + "\t" + str(ovcf.REF) + "\t" + str(ovcf.ALT[0]) + "\tFBRefAlleleCount=0\tFBReferenceAlleleQ=" + str(ovcf.QUAL) + "\tEFF_HGVS=OMICIAUNMAPPABLE:" + ovcf.ID + "\t" + "QUAL=" + str(ovcf.QUAL) + "\t" + "RSID=" + str(ovcf.ID) +  "\n"
                # print failreturn
         return failreturn
   
@@ -71,7 +71,7 @@ def AddOmicia(vcf,results,reskey):
     rsidreplace = m.group(1) + "(" + str(vcf.ID) + ")|(" + str(vcf.QUAL) + ")"
     #qual_fixed = re.sub("FBReferenceAlleleQ=\w+", qualreplace, results[reskey])
     qual_replaced = re.sub("EFF_EFFECT=\S+",rsidreplace,results[reskey])    
-    rsidadd = "\tRSID=" + str(vcf.ID) + "\t" + "QUAL=" + str(vcf.QUAL)
+    rsidadd = "\tRSID=" + str(vcf.ID) + "\t" + "QUAL=" + str(vcf.QUAL) + "\n"
     qual_rsid_added = qual_replaced + rsidadd    
     
     return qual_rsid_added 
