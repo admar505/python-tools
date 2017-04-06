a197c4928302e4390bdb63a98224a77f886a601d
Diego Martinez <diego@veritasgenetics.com>
2017-04-06 17:30:04 -0400

    switch to faidx, super duper fast

--- generate_synthetic_reads.py
+++ generate_synthetic_reads.py
@@ -12,35 +12,22 @@ import unittest
 from time import sleep
 from Bio import SeqIO
 import re
+import pyfaidx
 
 parser = argparse.ArgumentParser(description='HGVS-based Synthetic Read Generator')
 parser.add_argument("--mutations", help="file with mutations to generate synthetic reads for")
-parser.add_argument("--huref", help="Human Reference FASTA (matching BED)")
+parser.add_argument("--huref", help="Human Reference FASTA (matching BED) use the faidx (usually alled fai)")
 parser.add_argument("--output", help="prefix for file output")
 args = parser.parse_args()
 
 mutations_file = args.mutations
 
-def complement(s):
-    basecomplement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
-    letters = list(s)
-    letters = [basecomplement[base] for base in letters]
-    return ''.join(letters)
-def revcom(s):
-    return complement(s[::-1])
-def nuc_error(nuc1):
-	if (nuc1 == 'A'): nuc2 = random.sample(['C','G','T'],1)
-	elif (nuc1 == 'C'): nuc2 = random.sample(['A','G','T'],1)
-	elif (nuc1 == 'G'): nuc2 = random.sample(['A','C','T'],1)
-	elif (nuc1 == 'T'): nuc2 = random.sample(['A','C','G'],1)
-	return str(nuc2[0])
 
 # define the reference fasta file
 fasta = args.huref
-file_fasta = open(fasta,"r")
 
-fasta_dct = SeqIO.to_dict(SeqIO.parse(file_fasta,"fasta"))#seq is stored as a dict (hash)
-#print(fasta_dct.keys())
+fasta_dct = pyfaidx.Fasta(fasta)#seq is stored as a dict (hash)
+#fasta_dct = SeqIO.to_dict(SeqIO.parse(file_fasta,"fasta"))#seq is stored as a dict (hash)
 
 mutations_filehandle = open(mutations_file)
 mutations_lines = mutations_filehandle.readlines()
@@ -57,30 +44,31 @@ for mutations_line in mutations_lines:
 	malteration = mutations_data[4].strip()
 	moperator2 = mutations_data[5].strip()
 	malteration2 = mutations_data[6].strip()
-        old_seq = fasta_dct[mchrom].seq#whatshould this be? the file handle?
+        #old_seq = fasta_dct[mchrom].seq#whatshould this be? the file handle?
+        old_seq = fasta_dct[mchrom]#whatshould this be? the file handle?
         new_id = args.output + "." +  "-".join(mutations_data[0:7]).strip()  +  ".fa"
         OUT = open(new_id,"w")
 	new_seq = ""
 	if(moperator == "del"):
 	#	if (malteration.isalpha()):
 		if(moperator2 == "ins"):
-                    new_seq = old_seq[0:(int(mleft))]+old_seq[int(mleft)+(len(malteration)):len(old_seq)]
-                    new_seq = new_seq[0:mleft]+malteration2+new_seq[int(mleft):len(old_seq)]
+                    new_seq = str(old_seq[0:(int(mleft))])+str(old_seq[int(mleft)+(len(malteration)):len(old_seq)])
+                    new_seq = str(new_seq[0:mleft])+malteration2+str(new_seq[int(mleft):len(old_seq)])
                 else:
         #           print("OOHH NOOOEESS")
-                    new_seq = old_seq[0:(int(mleft))]+old_seq[int(mleft)+(len(malteration)):len(old_seq)]
+                    new_seq = str(old_seq[0:(int(mleft))])+str(old_seq[int(mleft)+(len(malteration)):len(old_seq)])
                 #print("INDEED . TREEEEUUUOOEEE")
                 #print(new_seq)
 
 	if(moperator == ">"):
-                new_seq = old_seq[0:(mleft - 1)]+malteration+old_seq[int(mleft):len(old_seq)]
+                new_seq = str(old_seq[0:(mleft - 1)])+malteration+str(old_seq[int(mleft):len(old_seq)])
 	if(moperator == "dup"):
 #		new_seq = old_seq[0:rel_pos-1]+malteration+old_seq[(rel_pos-1):len(old_seq)]
-                new_seq = old_seq[0:mleft]+malteration+old_seq[(mleft):len(old_seq)]
+                new_seq = str(old_seq[0:mleft])+malteration+str(old_seq[(mleft):len(old_seq)])
                 #print("DUP")
 	if(moperator == "ins"):
 #		new_seq = old_seq[0:rel_pos-1]+malteration+old_seq[(rel_pos-1):len(old_seq)]
-                new_seq = old_seq[0:mleft]+malteration+old_seq[int(mleft):len(old_seq)]
+                new_seq = str(old_seq[0:mleft])+malteration+str(old_seq[int(mleft):len(old_seq)])
 	#print(str(len(new_seq)))
 	#print(new_seq)
 	#print(str(len(old_seq)))
