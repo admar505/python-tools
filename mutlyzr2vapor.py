c90e61bd8ee5fd0267c3ab866bac318abc9901c2
Diego <diego@veritasgenetics.com>
2017-06-05 16:34:40 -0400

    Now, choose the alt transcripts, and fill them in

--- mutlyzr2vapor.py
+++ mutlyzr2vapor.py
@@ -4,12 +4,14 @@ import subprocess
 import re
 import csv
 
-parser = argparse.ArgumentParser(description='translate mutalyzer\'s batch output to VAPor input')
-parser.add_argument("--mut", help="batch output",required=True)
-parser.add_argument("--tr", help="good transcripts")
-parser.add_argument("--rs", help="the rsids you were looking for")
+parser = argparse.ArgumentParser(description='Translate mutalyzer\'s batch output to VAPor input')
+parser.add_argument("--mut", help="Batch output",required=True)
+parser.add_argument("--tr", help="Good transcripts")
+parser.add_argument("--rs", help="The rsids you were looking for")
+parser.add_argument("--vg", help="the number to start with for VG transcripts, as in 301 or something")
 args = parser.parse_args()
 
+vgTransID = args.vg
 mutfi = args.mut
 muts = open(mutfi,'r')
 transfi = args.tr
@@ -17,6 +19,15 @@ rsidsfi = args.rs
 targetrs = open(rsidsfi,'r')
 trans = open(transfi,'r')
 ##-----------defs--------##
+def selectAlt(choices,vgTransID):#array NR > XR > XM
+    goodalts = []
+    #print str(choices) + "   in select"
+    for vrsion in choices:
+        if vrsion[0]
+        #print vgTransID NEXT:: get the tacos into this space here.
+        #ok. lets do this, scan through, take best, and send it back, in best arrray version order.
+    return goodalts
+
 def nmCheck(ids,trans):
     arr=[]
     ncid = re.match('(NM_\d+)\.\d+:(.*)',ids)
@@ -24,6 +35,15 @@ def nmCheck(ids,trans):
         arr.append(ncid.group(1))
         arr.append(ncid.group(2))
         return arr
+
+
+def nrCheck(ids,trans):
+    arr=[]
+    ncid = re.match('(\w{2}_\d+)\.\d+:(.*)',ids)
+    arr.append(ncid.group(1))
+    arr.append(ncid.group(2))
+    return arr
+
 
 
 #------------main--------##
@@ -43,7 +63,8 @@ for rsid in muts:
     ncbi_ids = onerow[1]
     storelocale = {}#hash, key = version, val = position
     localeprint = ""
-    transhgvs = []
+    transhgvs = []#store NM ids
+    althgvs = [] #store alternate, as in NR,XR ids
     for ncbi in ncbi_ids.split('|'):
         #print ncbi
         if re.match('NC_',ncbi):
@@ -56,9 +77,36 @@ for rsid in muts:
         elif re.match('NM_',ncbi):
             if nmCheck(ncbi,goodtrans) is not None:
                 transhgvs.append(nmCheck(ncbi,goodtrans))
-#what if no NMIDS? figure that out::
-    for positions in transhgvs:
-        print onerow[0] +","+ positions[1] + "," + positions[0] + "," +   localeprint + "," + positions[0] + "||"+ positions[1]
+        elif re.match('NR_',ncbi) or re.match('XR_',ncbi) or re.match('XM_',ncbi):
+            if nrCheck(ncbi,goodtrans)is not None:
+                althgvs.append(nrCheck(ncbi,goodtrans))
+##Logic. if no NM, sort other, if none there, use VG_ and create the VG ids.
+    #if transhgvs is not None and len(transhgvs) > 0:
+    if transhgvs:
+        for positions in transhgvs:##This is valid, as thare T>G and T>C or A type mutations.
+            #print onerow[0] +","+ positions[1] + "," + positions[0] + "," +   localeprint + "," + positions[0] + "||"+ positions[1]
+            print ""
 
+    elif althgvs:
+        print str(althgvs) + "\tSTF"
+        altID = []
+        altID = selectAlt(althgvs,vgTransID)
+        print altID
+        if altID is not None:
+            for alt in altID:
+                print altID + ",CATCH NON-NM here"
+
+    else:
+        print "NEW TRANS HRERE"
 #loser bracket: ensure the rsids are all there.
+
+
+
+
+
+
+
+
+
+
 
