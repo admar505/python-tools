0b0f8f0cc10b9c0f406dad1cc11f54c2eb85b8d4
Diego <diego@veritasgenetics.com>
2017-06-07 15:43:45 -0400

    done

--- mutlyzr2vapor.py
+++ mutlyzr2vapor.py
@@ -3,12 +3,12 @@ import argparse
 import subprocess
 import re
 import csv
+import numpy
 
 parser = argparse.ArgumentParser(description='Translate mutalyzer\'s batch output to VAPor input')
 parser.add_argument("--mut", help="Batch output",required=True)
 parser.add_argument("--tr", help="Good transcripts")
-parser.add_argument("--rs", help="The rsids you were looking for")
-parser.add_argument("--vg", help="the number to start with for VG transcripts, as in 301 or something")
+parser.add_argument("--vg", help="the number to start with for VG transcripts, as in 301 or something",type = int)
 args = parser.parse_args()
 
 newtrans = open('NEW.vapor.transcripts.txt','w+')
@@ -16,8 +16,6 @@ vgTransID = args.vg
 mutfi = args.mut
 muts = open(mutfi,'r')
 transfi = args.tr
-rsidsfi = args.rs
-targetrs = open(rsidsfi,'r')
 trans = open(transfi,'r')
 ##-----------defs--------##
 
@@ -26,31 +24,24 @@ def varPrint(row1,pos,local):#HERE:expand to all vartypes, and print out.
                              #(het, homovar, wt+)
     np = re.match('(\w\..*?\d+)(\w+)\>(\w+)',pos[1])
     npwt = np.group(1) + np.group(2) + "="
-    print row1[0] +","+ pos[1] + "," + pos[0] + "," +   local + "," + pos[0] + "||"+ pos[1]
-    print row1[0] +","+ npwt + "," + pos[0] + "," +   local + "," + npwt + "||"+ pos[1]
-    print row1[0] +","+ pos[1] + ":0/1," + pos[0] + "," +   local + "," + pos[0] + ":0/1||"+ pos[1]
+    print row1[0] +","+ pos[1] + "," + pos[0] + "," +   local + "," + pos[1] + "||"+ pos[0]
+    print row1[0] +","+ npwt + "," + pos[0] + "," +   local + "," + npwt + "||"+ pos[0]
+    print row1[0] +","+ pos[1] + ":0/1," + pos[0] + "," +   local + "," + pos[1] + ":0/1||"+ pos[0]
 
 
-def newTrans(vgid,rs,newtrans):#title,trans,version,gene
-    print "should WRITE HERE"
-    newid = "VG_000" + vgid
-    vgid += 1
-    newtrans.write('\"' + rs + " " + newid + '\"\,\"' + newid + '\"\,\"1\"\,\"' + rs + '\"')
-
-    return vgid
 
 
 
 def bestXX(trarr):#pick closest TR
     toreturn = {}
     for tr in trarr:
-        pos = re.match('n.(\d+)',tr[1])
-        toreturn[pos.group(1)] = tr
+        pos = re.match('[cn].[*-]{0,1}(\d+)',tr[1])
+        toreturn[int(pos.group(1))] = tr
     keyssorted = sorted(toreturn.keys())
-    #print str(keyssorted)
     return toreturn[keyssorted[0]]
 
-def selectAlt(choices,vgTransID,rsid_current,transfile):#array NR > XR > XM
+def selectAlt(choices,rsid_current,transfile):#array NR > XR > XM
+
     goodalts = []
     NR = []
     XR = []
@@ -71,15 +62,13 @@ def selectAlt(choices,vgTransID,rsid_current,transfile):#array NR > XR > XM
 
     #now, sort through the stuff.
     if len(NR) > 0:
-        goodalts.append(NR)
-    elif len(XR) > 0:
-        goodalts.append(bestXX(XR))
+        goodalts.append(bestXX(NR))
     elif len(XM) > 0:
         goodalts.append(bestXX(XM))
+    elif len(XR) > 0:
+        goodalts.append(bestXX(XR))
     else:
-        vgTransID = newTrans(vgTransID,rsid_current,transfile)
-        goodalts.append(vgTransID)
-    print str(goodalts) + "\tGOODALTS"
+        goodalts.append(None)
     return goodalts
 
 
@@ -99,15 +88,22 @@ def nrCheck(ids,trans):
     arr.append(ncid.group(2))
     return arr
 
+def newTrans(vgid,rs,newtrans):#title,trans,version,gene: return array of hgvs for new var.
+    newid = "VG_000" + str(vgid)
+    newtrans.write('\"' + rs + " " + newid + '\",\"' + newid + '\",\"1\",\"' + rs + '\"\n')
+    return newid
+
+def newHGVS(place,vgTransID,rsid, newtrans):
+    newvar = []
+    newvar.append(newTrans(vgTransID,rsid,newtrans))
+    newvar.append('c.10' + place)
+    return newvar
+
+
 def perf(ind):
     return "NULL"
 
 #------------main--------##
-seentit = {}
-rsids = {}
-for rsid in targetrs.next():
-    rsids[rsid] = rsid
-
 goodtrans = {}
 for tr in trans:
     m = re.match('(NM_\d+)\.(\d+)',tr)
@@ -115,17 +111,18 @@ for tr in trans:
 
 for rsid in muts:
     onerow = rsid.split()
-    seentit[onerow[0]] = onerow
     ncbi_ids = onerow[1]
     storelocale = {}#hash, key = version, val = position
     localeprint = ""
     transhgvs = []#store NM ids
     althgvs = [] #store alternate, as in NR,XR ids
+    failhgvs = ""#only use if no hgvs is available, ie, only if
     for ncbi in ncbi_ids.split('|'):
         #print ncbi
         if re.match('NC_',ncbi):
-            m = re.match('NC_0{1,6}(\d+)\.(\d+):g.(\d+)',ncbi)
+            m = re.match('NC_0{1,6}(\d+)\.(\d+):g.(\d+)(.*)',ncbi)
             storelocale[int(m.group(2))] = m.group(3)
+            failhgvs = m.group(4)
             if len(storelocale) == 2:
                 storek =  storelocale.keys()
 
@@ -140,27 +137,22 @@ for rsid in muts:
     #if transhgvs is not None and len(transhgvs) > 0:
     if transhgvs:
         for positions in transhgvs:##This is valid, as thare T>G and T>C or A type mutations.
-            perf(1)
-            #varPrint(onerow,positions,localeprint)
+            #perf(1)
+            varPrint(onerow,positions,localeprint)
 
     elif althgvs:
         altID = []
-        altID = selectAlt(althgvs,vgTransID,onerow[0],newtrans)
+        altID = selectAlt(althgvs,onerow[0],newtrans)
         #print altID
         if altID is not None:
-            for alt in altID:
-                varPrint(onerow,positions,localeprint)
-                #print str(altID) + ",CATCH NON-NM here"
+            c = 0
+            while c < len(altID):
+                varPrint(onerow,altID[c],localeprint)
+                c += 1
 
-    #else:NOW: this is working correctly here.
-    #    print "NEW TRANS HRERE"
-#loser bracket: ensure the rsids are all there.
-
-
-
-
-
-
+    else:#NOW: this is working correctly here.)
+        varPrint(onerow,newHGVS(failhgvs,vgTransID,onerow[0],newtrans),localeprint)
+        vgTransID += 1
 
 
 
