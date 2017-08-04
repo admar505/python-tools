f8b3fc106577daca1c9b694feb9ac1814402b942
diego <diego_isi@veritasgenetics.com>
2017-08-03 21:57:53 -0400

    write works! on to adding snpEff and VEP

--- testGeneImpacts.py
+++ testGeneImpacts.py
@@ -8,7 +8,7 @@ import csv
 from subprocess import call
 import random
 #Test if geneImpacts does job I want, and can add to VGR record.
-#is in the planning doc, there will be two phases. one for collecting data, the second for deciding which data to keep. and, a third for adding 
+#is in the planning doc, there will be two phases. one for collecting data, the second for deciding which data to keep. and, a third for adding
 #freq data, and you know, a fourth for filtration, and a fifth for ACMG incidental findings.
 #
 #
@@ -27,15 +27,15 @@ def addNewRecord(varrec):
     newRes = vgr.model._Record(varrec.CHROM,varrec.POS,varrec.REF,varrec.ALT,{})
     newRes.INFO['QUAL'] = varrec.QUAL
     #newRes.add_info
-    print newRes.INFO
+    #print newRes.INFO
     return newRes
-    
+
 def addAnn2Array(var):
-    
 
 
 
-    return 1    
+
+    return 1
 
 
 
@@ -43,12 +43,11 @@ def addAnn2Array(var):
 resData = {}#key is record: val is the VGR line!
 for variant in vcf_full:
     resData[str(variant.CHROM) + str(variant.POS) +  str(variant.REF) + str(variant.ALT)] = addNewRecord(variant)
-    print variant.INFO['ANN']
+    #print variant.INFO['ANN']
 
 
 for row in resData:
-    print row
-    newresults.write_record(row)
+    newresults.write_record(resData[row])
 
 
 #for rec in resData:
