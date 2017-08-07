fe069bea02e6d963d6bacdcc89513eeeb2270e85
Diego Nucleo Martinez <diego@veritasgenetics.com>
2017-08-07 16:36:12 -0400

    added in both VEP and snpEff support

--- testGeneImpacts.py
+++ testGeneImpacts.py
@@ -8,43 +8,44 @@ import csv
 from subprocess import call
 import random
 #Test if geneImpacts does job I want, and can add to VGR record.
-#is in the planning doc, there will be two phases. one for collecting data, the second for deciding which data to keep. and, a third for adding
-#freq data, and you know, a fourth for filtration, and a fifth for ACMG incidental findings.
-#
+#Plan: take
 #
 parser = argparse.ArgumentParser(description="VGR and geneimpacts library test!!")
-parser.add_argument("--efd",help="fullvcf file",required=True)
+parser.add_argument("--efd",help="snpEff annotated vcf file",required=True)
+parser.add_argument("--vep",help="VEP annotated vcf file",required=True)
 
 args = parser.parse_args()
 efdfi = args.efd
-
-vcf_full = vcf.Reader(open(efdfi,'r'))
-
+vepfi = args.vep
+efd_full = vcf.Reader(open(efdfi, 'r'))
+vep_full = vcf.Reader(open(vepfi, 'r'))
 newresults = vgr.Writer(open('test.COMPLETE.txt',"w"))
 #-----------------here by DEFSgONS!!-------------------------*
 
-def addNewRecord(varrec):
+def addNewRecord(varrec):#Returns a vgr  record:
     newRes = vgr.model._Record(varrec.CHROM,varrec.POS,varrec.REF,varrec.ALT,{})
     newRes.INFO['QUAL'] = varrec.QUAL
+    newRes.INFO['FB_GenoType'] = varrec.sample['unknown']
     #newRes.add_info
     #print newRes.INFO
     return newRes
 
-def addAnn2Array(var):
 
 
 
-
-    return 1
-
-
-
+    
+    
+    
+    
+    
+    
 ##<---------------++++++++++ MAIN ++++++++++++------------->##
 resData = {}#key is record: val is the VGR line!
-for variant in vcf_full:
+for variant in vep_full:
+    #resData[str(variant.CHROM) + str(variant.POS) +  str(variant.REF) + str(variant.ALT)] = addNewRecord(variant)
     resData[str(variant.CHROM) + str(variant.POS) +  str(variant.REF) + str(variant.ALT)] = addNewRecord(variant)
     #print variant.INFO['ANN']
-
+    
 
 for row in resData:
     newresults.write_record(resData[row])
