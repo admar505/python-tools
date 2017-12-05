9ae06caf3193f27ac13cb6f5b7f767a1a1e96879
Diego <diego@veritasgenetics.com>
2017-12-05 17:38:03 -0500

    work to make the handling of variants, especially with ambiguous calls and tainted with minor alleles that are really just noise

---
+++ AltObject/altobject.py
@@ -0,0 +1,6 @@
+class AltObj(object):
+    #create an object that has the var in a pipeline
+    def __init__(self):	
+        self.ab = 0.0 #the allele balance of this var
+        self.base  #the 
+        self.pos
