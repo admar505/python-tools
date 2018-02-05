0235f36d3b5122248c8bf437ee5162cd1cf7ed68
Diego <diego@veritasgenetics.com>
2018-02-05 16:40:09 -0500

    calc GT ratios step 1, gather GL

--- detectMissMergedSamplesinVCF.py
+++ detectMissMergedSamplesinVCF.py
@@ -4,7 +4,7 @@ import subprocess,sys,os,re,fileinput,argparse,math
 import vcf
 sys.path.append('/home/nucleo/python-tools/AltObject')
 
-import loadaltdats
+from loadaltdats import detGenoType
 
 parser = argparse.ArgumentParser(description="adds the position info into either the ID col or in INFO")
 parser.add_argument("--vcf",help="the vcf file, has to be bgzipped and indexed",required=True)
@@ -22,32 +22,60 @@ repeatsfile =args.rpt
 
 vcf_full = vcf.Reader(open(vcffi,'r'))
 #repair for one sample and not two.
-tempheader = open('vcf.tmp','w')
-headerrewritten = open('tmp.vcf','w')
-repeats = open(repeatsfile,'r')
+#tempheader = open('vcf.tmp','w')
+#headerrewritten = open('tmp.vcf','w')
+if repeatsfile:
+    repeats = open(repeatsfile,'r')
 
 #--subs--#
+def homorhet(gt):#if homo, ret true
+    homo =  False
+    #print gt
+    check = gt.split(',')
+    if check[0] == check[1]:
+        homo = True
 
+    return homo
 
 #--main--#
 
+none = 0
+homo = 0
+major_alt = 0
+minor_alt = 0
+extra_alts = 0
 
-#for var in vcf_full:
+
+for var in vcf_full:
     #print var.ALT[0]
     #print var.CHROM + "\t" + str(var.POS) + "\t" +  str(var.samples[0]['GL'])
-    #altDict = {}#hold alternates for
+    gtglpair = {}
+    gtglpair = detGenoType(var)
+    how_many_gts = gtglpair.assGT_GL.keys()
+    c = 0
+    while c < len(how_many_gts):
+        gt = how_many_gts[c]
+        if homorhet(gt):
+            homo += gtglpair.assGT_GL[gt]
 
-    #check_for_none = var.ALT[0]
+        elif c == 1:
+            major_alt += gtglpair.assGT_GL[gt]
+        elif c == 2:
+            major_alt += gtglpair.assGT_GL[gt]
+        else:
+            extra_alts += gtglpair.assGT_GL[gt]
 
-    #if check_for_none is not None:
-       # print var.samples[0]['GT']
-            #from here, toss the variant into the alt class, and create alt objects.
+
+print homo
+print major_alt
+print minor_alt
+print extra_alt
 
 
 
-alts = loadaltdats.detRepeats(vcf_full,repeats)
+#alts = loadaltdats.detRepeats(vcf_full,repeats)
 
-print alts.countRepeats
+#print alts.countRepeats
 
     #print alts.countRepeats(repeats)
     #for alternatevars in alts:
