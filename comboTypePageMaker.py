4c2743afa45fb5d8c94c54f00f4ac51263348de5
Diego Nucleo Martinez <diego@veritasgenetics.com>
2018-02-27 16:22:50 -0500

    Trying to get the combotype hasher done

--- comboTypePageMaker.py
+++ comboTypePageMaker.py
@@ -2,6 +2,7 @@
 import argparse
 import subprocess
 import re
+import sys
 
 parser = argparse.ArgumentParser(description='for building pages from combos, input trans:hgvs;trans:hgvs;trans:hgvs\tlabelID')
 parser.add_argument("--mut", help="Batch output",required=True)
@@ -20,6 +21,7 @@ muts = open(mutfi,'r')
 
 def varPrint(row1,pos,local):#HERE:expand to all vartypes, and print out.
                              #(het, homovar, wt+)
+                             #choose most common transcript to use a trans. FOR NEW version
     #print pos[1]
     #adjust to account for del. Stupid CFTR.
 
@@ -53,34 +55,70 @@ def varPrint(row1,pos,local):#HERE:expand to all vartypes, and print out.
     print row1[0] +","+ pos[1] + ":0/1," + pos[0] + "," +   local + "," + pos[1] + ":0/1||"+ pos[0]
 
 
-def hgvsMaker(combo):
+
+def hgvsMaker(combo):#makes the HGVSses for all three types
 
     hgvsses = []
-    for variant in combo.split(";"):
-        try:
-            cap = re.search('([NMXR]{2}\_\d+\.?\d?)\:(c.[-+*]{0,1}\d+.*?)([*_\-\>del]{1,4})(.*)',variant)
-
-            print cap.group(1)
-            hgvsses.append(cap.group(0))
-            #hgvsses.append(cap.group(1) + ":" + $2 + $3 + $4)#WT+
-            hgvsses.append(cap.group(1) + ":[" + cap.group(2) +  cap.group(3) + cap.group(4) + "];[" + cap.group(2) + "=]")#HET
+    try:
+        cap = re.search('([NMXR]{2}\_\d+\.?\d?)\:c.([-+*]{0,1}\d+.*?)([*\-\>del]{1,4})(.*)',combo)
 
 
-        except NoneType:
-
-            print "ERROR:HGVS parsing is blown, correct please"
-
-    return
+        hgvsses.append(cap.group(0))
+        hgvsses.append(cap.group(1) + ":c." + cap.group(2) + "=" )#WT+
+        hgvsses.append(cap.group(1) + ":c.[" + cap.group(2) +  cap.group(3) + cap.group(4) + "];[" + cap.group(2) + "=]")#HET
 
 
+    except NoneTypeError:
 
-def perf(ind):
-    return "NULL"
+        print "ERROR:HGVS parsing is blown, correct please"
+
+    return hgvsses
+
+
+
+
+def comboExpander(variantarray):##maker of the combos, so for all in, produces the uuid and sends to printer
+    linevals = []               #length of arrays length of vals, and do a while less than, or for each with index.
+                                #HINT: have starter, and then a follower. try the recursor. ie, first row is used to key off of,
+                                #then, all other rows/cols have a unified structure.
+    for variants in variantarray[0]:
+        linevals.append(variants)
+        vrow = 1
+        vcols = 0
+        while vcols + 1 < len(variantarray[vrow]):
+            vrow = 1
+            print str(vcols) +  "\tcols\t" + str(vrow) + "\tvrows\t" + str(len(variantarray))
+
+
+            while vrow  < (len(variantarray) - 1):
+            #for vrows  in range(len(variantarray)):
+                print variantarray[vcols][vrow]
+                linevals.append(variantarray[vrow][vcols])
+
+                print str(vcols) +  "\tcols\t" + str(vrow) + "\tvrows\t" + str(len(variantarray))
+                vrow += 1
+
+            vcols += 1
+
+        print "\t".join(linevals)
+        linevals = []
+
+
+
+    #for vareach in variantarray:
+    #    for var in vareach:
+    #        linevals.append(var)
+
+
+
+
+
+
 
 #------------main--------##
 
 for combo_ln in muts:
-    combo_hgvs_lst = []
+    combo_hgvs_lst = []#contain the types. each row is one variant to use,
     combo_array = combo_ln.split()
     try:
         combos = combo_array[0]
@@ -90,8 +128,33 @@ for combo_ln in muts:
         print "ERROR: Input file is wrong"
 
     #make combos. so, enumerate the types. then, expand them do a triple for each.
+    for combination in combos.split(";"):
+        combo_hgvs_lst.append(hgvsMaker(combination))
 
-    combo_hgvs_lst.append(hgvsMaker(combos))
+    print combo_hgvs_lst
+    comboExpander(combo_hgvs_lst)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
