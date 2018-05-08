16cb436c1bf7ea12c072ac1749a49e426c8eadd4
LordOfTheTacos <admar505@gmail.com>
2018-05-08 10:18:38 -0400

    prepCap, cant get files to write correctly to disk

--- prepCap.py
+++ prepCap.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 import sys,os,re,fileinput,argparse
 import vcf
+from subprocess import call
 import csv
 import random
+sys.path.append("/home/nucleo/lib/PyVGRes")
+import vgr
+
 parser = argparse.ArgumentParser(description="select infor from VCF")
 parser.add_argument("--vcf",help="vcf file, bgzipped and tabixed as -p vcf",required=True)
 parser.add_argument("--list",help="lost to search through, in CAP format",required=True)
@@ -17,63 +21,52 @@ necessary_vars = csv.DictReader(open(varslst,'r'),delimiter="\t")
 #parse results in a map or dict, or what??
 
 #-------------------------------------here by DEFSgONS!!----------------------------------*
-def anyNone(rets):
-
-    size = len(rets)
-    none_ct = size  #set to size, reduce as nones go
+def recallVar(fudge):
+    print "FUCK"
 
 
-    for tagkey in rets:
-        checkfornone = re.match('.*?\[None\].*',str(rets[tagkey]))#LOGIC: if there is a none check for none will be NOT NONE??
 
-        try:
-
-            if checkfornone is  None:
-                none_ct = none_ct - 1 #decided to use as --, as it is more sensible.
-
-        except (AttributeError, IndexError) as e:
-            dn = open(os.devnull,'w')
-
-    final_return = None
-
-    if none_ct < size:##indicates that no NONE vals were found in entirety.
-        final_return = rets
-
-    return final_return
+def writeFinal():
+    return None
 
 
-def getTags(tags, varset):
-    retval = {}
 
-    for tagval in tags:
-
-        if tagval in varset:
-            #retval.append(tagval +  '=' + str(varset[tagval]))
-            retval[tagval] = varset[tagval]
-
-    #make a loop or def() that checks of at least one is not none.
-
-    return_final = anyNone(retval)
-    #print return_final
-    return return_final
+def mergeNWrite(vgrfile,caps):
+    complete = vgr.Reader(open(vgrfile,'r'))
 
 
-def kickAnnotations(vcfln,capln):
+
+
+
+    return None
+
+
+def kickAnnotations(vcfln,capln,vcf_4_header):
     returnvals = []
     #command = "/vbin/GoPipeRUN/goVarAnnotateAndID.sh" + rsid
     #call(["/vbin/GoPipeRUN/goVarAnnotateAndID.sh",rsid])
+    rsid = capln['Chr'] + "."  + capln['Pos']
+    vcf_write = vcf.Writer(open(rsid + '.Merged.vcf','w'),vcf_4_header)
+    vcf_write.write_record(vcfln)
+    sys.stdout.flush()
+
     effout = open(rsid + '.efd.vcf',"w")
-    call(['java -jar /vbin/snpEff/snpEff.jar eff -i vcf -csvStats -hgvs hg19 ' + rsid + '.Merged.vcf'],shell=True,stdout=effout)
+    errout = open(rsid + '.stderrr',"w")
 
-    call(['/vbin/ensembl-tools-release-78/scripts/variant_effect_predictor/variant_effect_predictor.pl --force_overwrite --vcf -i ' + rsid + '.Merged.vcf -o ' +rsid + '.VEP.vcf --everything --species homo_sapiens --cache --refseq --offline --assembly GRCh37 --fasta /vbin/ref/hg19.nochr.fa'],shell=True)
+    call(['java -jar /vbin/snpEff/snpEff.jar eff -i vcf -csvStats -hgvs hg19 ' + rsid + '.Merged.vcf'],shell=True,stdout=effout,stderr=errout)
+    sys.stdout.flush()
+    effout.close()
+
+    call(['/vbin/ensembl-tools-release-86/scripts/variant_effect_predictor/variant_effect_predictor.pl --force_overwrite --vcf -i ' + rsid + '.Merged.vcf -o ' + rsid + '.VEP.vcf --dir /vbin/ensembl-tools-release-86/  --everything --species homo_sapiens --cache --refseq --offline --assembly GRCh37 --fasta /vbin/ref/hg19.nochr.fa'],shell=True,stderr=errout)
+    sys.stdout.flush()
     complete = open(rsid + '.COMPLETE.txt',"w")
-    call(['/vbin/Perl/matchPathsAndMergeCallers.2.pl  --evs /ref/EVS_AF.vcf -p /ref/FULL.GENOME.lookUP.txt -eff ' + rsid + '.efd.vcf -vep ' + rsid + '.VEP.vcf -trn /ref/WG_good_genes.lst  -exac /ref/ExAC.r0.3.1.sites.af.vcf  -hgmd /ref/Homo_sapiens.HGMD.hg19.chr.vcf'],shell=True,stdout=complete)
+
+    call(['/vbin/Perl/matchPathsAndMergeCallers.2.pl  --evs /ref/EVS_AF.vcf -p /ref/FULL.GENOME.lookUP.txt -eff ' + rsid + '.efd.vcf -vep ' + rsid + '.VEP.vcf -trn /ref/WG_good_genes.lst  -exac /ref/ExAC.r0.3.1.sites.af.vcf  -hgmd /ref/Homo_sapiens.HGMD.hg19.chr.vcf'],shell=True,stdout=complete,stderr=errout)
+
+    mergeNWrite(rsid + '.COMPLETE.txt',capln)
 
 
 
-
-
-    return "\t".join(returnvals)
 
 
 
@@ -104,7 +97,7 @@ def getVCFLine(varln,vfile):
 for needed in necessary_vars:
     extracted = getVCFLine(needed,vcf_full)
     if extracted is not None:
-        kickAnnotations(extracted,needed)
+        kickAnnotations(extracted,needed,vcf_full)
 
 
     else:
