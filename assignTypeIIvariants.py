c0c148940776015316072658653eeef2188ee9de
Diego <diego@veritasgenetics.com>
2018-05-21 16:29:13 -0400

    assignTypeII now has a valid check to see if AB agrees. next, decide which to do, return LC or return WT

--- assignTypeIIvariants.py
+++ assignTypeIIvariants.py
@@ -15,7 +15,7 @@ parser.add_argument("--answer",help="the mapping of variant to VAPor page")
 parser.add_argument("--vcf",help="VCF file, bgzipped, indexed with tabix -p vcf")
 parser.add_argument("--fullvcf",help="FULL genome VCF file, bgzipped, indexed with tabix -p vcf")
 parser.add_argument("--combo",help="combination types",action='append')
-
+parser.add_argument("--ABthreshold",help="this is a value at which to trust allele ballance, default is 15",default=0.15)
 
 
 
@@ -63,31 +63,63 @@ def gtCallOfficial(genocall):#return variant-caller asserted GT #IAMHERE
     return returnable_string
 
 
-def assignFinalGT(callfb,AB,qual):#ok, so, some logic, if the gt gl all work,
-                                         #and the qqual is above threshold, and the AB is good, give it a blam.
-                                         #see if all agree. if so, see if the call is homo, het or wt
+def checkGLtoAB(gl,ab,ql,alt,ref,callao): #return True if GL and AB vals agree on call.
+                                   #approach: verify that GTbyGL is either WT, or, is high AB alt
+    itisgood = False
+    #print str(ab) + "\t" + str(alt) + "\t" + str(gl)
+
+    isinWT = False
+    isValidAB = False
+
+    for nucl in gl.split(","):
+        if nucl == ref:
+
+            isinWT = True
+
+    variant = 0
+
+    while variant < len(ab):
+
+        if float(ab[variant]) > float(args.ABthreshold):
+            print str(alt[variant]) +  "\tis this in the variant and everything here\t" + str(gl)
+
+            if str(alt[variant]) in str(alt):
+                isValidAB = True
+
+        variant += 1
+
+    if (isinWT is True and isValidAB is True) or (isValidAB is True and callao.amIHOMO is True):
+        itisgood = True
+
+    return itisgood
+
+def assignFinalGT(callAO,var_fb):#ok, so, some logic, if the gt gl all work,
+    AB = var_fb.INFO['AB']               #and the qqual is above threshold, and the AB is good, give it a blam.
+    qual = var_fb.QUAL                   #see if all agree. if so, see if the call is homo, het or wt
                                          ##then, can pull the lines here from the answer bed: homohgvs  homourl hethgvs heturl  wthgvs  wturl
                                          #Also: ensure, with the rsid, that the call is valid.
-    asserted_gt = gtCallOfficial(callfb)
+    asserted_gt = gtCallOfficial(callAO)
 
-    best_gtGL = getBestGL(callfb.assGT_GL)
+    best_gtGL = getBestGL(callAO.assGT_GL)#this value stores what should be returned. test all against this value.
 
-
-
-
-
-    for vals in callfb.defGT_Dict:
-        print str(callfb.defGT_Dict[vals]) +"\tVALS\t"+ str(vals) + "\tbest\t"  + best_gtGL + "\t" + str(AB) + " assertedGT " +  str(asserted_gt)
+    if checkGLtoAB(best_gtGL,AB,qual,var_fb.ALT,var_fb.REF,callAO) is True:
+        print "it is"
+        print str(callAO.amIHOMO) + "\tHOMO:"
 
 
 
+    for vals in callAO.defGT_Dict:
+        print str(callAO.defGT_Dict[vals]) +"\tVALS\t"+ str(vals) + "\tbest\t"  + best_gtGL + "\t" + str(AB) + " assertedGT " +  str(asserted_gt)
 
+
+
+    return best_gtGL
 
 def returnWT(wtcall,answerline):
     print answerline['wthgvs'] + "\t" + answerline['wturl']
 
-def determineCall(varobj,targ): #this will be the beginning of determining the call.
-
+def determineCall(varobj,targ): #This will be the beginning of determining the call.
+                                #step TWO
 
 
     for variant in varobj:      #should this differentiate between dels and snps? lets see here.
@@ -104,7 +136,7 @@ def determineCall(varobj,targ): #this will be the beginning of determining the c
             try:
                 print "CALL IS GOOD " + str(callobj.defGT_Dict)
                 print "here is this " + str(callobj.assGT_GL)
-                assignedGT = assignFinalGT(callobj,variant.INFO['AB'],variant.QUAL)
+                assignedGT = assignFinalGT(callobj,variant)
 
 
             except AttributeError:
@@ -119,7 +151,7 @@ def determineCall(varobj,targ): #this will be the beginning of determining the c
 
 for bed in bedfi:#as csvDictReader
 
-    #FIRST: collapse ithe
+    #FIRST: collapse ithe rsids into possibles, example A-C or A-T for the same.
     #try:#need to pass the specific bed line that is target
 
         variant = resvcf.fetch(str(bed['chr']),int(bed['start']),int(bed['stop']))
