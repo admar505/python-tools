d10579a69b6cfbd63cd1a3f00d467e713841ac30
Diego <diego@veritasgenetics.com>
2019-06-05 17:01:45 -0400

    to validate the HGVSs that come from mutalyzer.py

--- validateHGVSandRSIDmappings.py
+++ validateHGVSandRSIDmappings.py
@@ -14,102 +14,24 @@ args = parser.parse_args()
 newtrans = open('NEW.vapor.transcripts.txt','w+')
 mutfi = args.mut
 vgrfi = args.vg
-muts = csv.Reader(open(vgrfi,'r'),delimiter='\t')
+vgr = csv.DictReader(open(vgrfi,'r'),delimiter='\t')
+muts = open(mutfi,'r')
 ##-----------defs--------##
 
+def checkMatch(inc_hgvs,inc_chr,inc_pos,matchd):#newtranslated_pos,dictionary
 
+    found = False
 
-def bestXX(trarr):#pick closest TR
-    toreturn = {}
-    for tr in trarr:
-        pos = re.match('[cn].[*-]{0,1}(\d+)',tr[1])
-        toreturn[int(pos.group(1))] = tr
-    keyssorted = sorted(toreturn.keys())
-    return toreturn[keyssorted[0]]
+    if inc_hgvs in matchd:
+        chk_val = "chr" + inc_chr + ":" + inc_pos
 
-def selectAlt(choices,rsid_current,transfile):#array NR > XR > XM
+        if chk_val == matchd[inc_hgvs]:
+            found =True
 
-    goodalts = []
-    NR = []
-    XR = []
-    XM = []
-    #print str(choices) + "   in select"
-    for ind in choices:
-        #print vgTransID NEXT:: get the tacos into this space here.
-        #ok. lets do this, scan through, take best, and send it back, in best arrray version order.
-        if re.match('NR',ind[0]):
-            NR.append(ind)
-            #print ind
-        elif re.match('XR',ind[0]):
-            XR.append(ind)
-            #print ind
-        elif re.match('XM',ind[0]):
-            XM.append(ind)
-            #print ind
-
-    #now, sort through the stuff.
-    if len(NR) > 0:
-        goodalts.append(bestXX(NR))
-    elif len(XM) > 0:
-        goodalts.append(bestXX(XM))
-    elif len(XR) > 0:
-        goodalts.append(bestXX(XR))
-    else:
-        goodalts.append(None)
-    return goodalts
-
-
-def nmCheck(ids,trans):
-    arr=[]
-    ncid = re.match('(NM_\d+)\.\d+:(.*)',ids)
-    if ncid.group(1) in trans.keys():
-        arr.append(ncid.group(1))
-        arr.append(ncid.group(2))
-        return arr
-
-
-def nrCheck(ids,trans):
-    arr=[]
-    ncid = re.match('(\w{2}_\d+)\.\d+:(.*)',ids)
-    arr.append(ncid.group(1))
-    arr.append(ncid.group(2))
-    return arr
-
-def newTrans(vgid,rs,newtrans):#title,trans,version,gene: return array of hgvs for new var.
-    newid = "VG_000" + str(vgid)
-    newtrans.write('\"' + rs + " " + newid + '\",\"' + newid + '\",\"1\",\"' + rs + '\"\n')
-    return newid
-
-def newHGVS(place,vgTransID,rsid, newtrans):
-    newvar = []
-    newvar.append(newTrans(vgTransID,rsid,newtrans))
-    newvar.append('c.10' + place)
-    return newvar
-
-
-def RSIDadder(hgv):
-
-    m = re.search('(\w+)([>delinsdup]{1,3})(\w+)',hgv)#absolutely RETARDED, captures min not max
-
-    if m.group(2) == 'l':
-        rsidadder = "-" + "del-" + m.group(3)
-
-    elif m.group(2) == 's':
-        rsidadder = "-" + "ins-" + m.group(3)
-
-    elif m.group(2) == 'u':
-        rsidadder = "-" + "dup-" + m.group(3)
-
-    else:
-        rsidadder = "-" + m.group(1) + "-" + m.group(3)
-
-    return rsidadder
+    return found
 
 
 
-
-def perf(ind):
-    return "NULL"
 
 #------------main--------##
 
@@ -120,32 +42,39 @@ lookup = {}
 for hg in vgr:
     lookup[hg['hgvs']] = hg['chr'] + ":" + hg['pos']
 
-
+print("original table loaded.....")
 
 
 for resln  in muts:
 
-    #onerow = rsid.split()
-    #ncbi_ids = onerow[1]
-    storelocale = {}#hash, key = version, val = position
-    localeprint = ""
-    transhgvs = []#store NM ids
-    althgvs = [] #store alternate, as in NR,XR ids
-    failhgvs = ""#only use if no hgvs is available, ie, only if
+    isitgood = None
 
-    for ncbi in ncbi_ids.split('|'): #keep this, it has NC line parsing.
-        #print ncbi
+    storelocale = {}
+    #ok, have to check for valid values, I think the the check for NC might be enough.
+    #then, if there is a valid NC value, we can proceed. if nothing at end, then kick out
+    #hgvs with a statement of invalid in condition.
+
+    resvalues = resln.split('\t')
+
+
+    for ncbi in resvalues[1:]:  #keep this, it has NC line parsing.
+                                        #sorting is done by version magic.
         if re.match('NC_',ncbi):
             m = re.match('NC_0{1,6}(\d+)\.(\d+):g.(\d+)(.*)',ncbi)
-            storelocale[int(m.group(2))] = m.group(3)
-            failhgvs = m.group(4)
+            storelocale[int(m.group(2))] = m.group(3) #loads the version, so that it can be sorted on.
+            #print(m.group(3) + "\t" + m.group(2) + "\t" + m.group(1))
+            #print("match " + m.group(2) )
 
-            if len(storelocale) == 2:
-                storek =  storelocale.keys()
+            if len(storelocale.keys()) == 1:
+                storek =  storelocale.keys()#this statement, will autosort??
 
-                localeprint = m.group(1) +"," + storelocale[storek[0]] +"," + m.group(1) + "," + storelocale[storek[1]]
+                isitgood = checkMatch(resvalues[0],m.group(1),storelocale[storek[0]],lookup)
 
+    if isitgood == True:
+        print("MATCH_FOUND\t" + resvalues[0] + "\tchr" + m.group(1) + "\t" + storelocale[storek[0]])
 
+    else:
+        print("NOT_FOUND or NOT_RETURNED\t" + resln )
 
 
 
