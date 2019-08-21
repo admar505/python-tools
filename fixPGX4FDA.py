01f36174cad553cad7f57929d9d849a6173c99e9
NUcleo,King of the DNA Tacos <nucleo@veritasgenetics.com>
2019-08-21 16:48:59 -0400

    need to now handle the NAT2 and the otherstuff

--- fixPGX4FDA.py
+++ fixPGX4FDA.py
@@ -6,10 +6,13 @@ parser = argparse.ArgumentParser(description="for TypeII RESULTS files, compare 
 parser.add_argument("--vapor",help="Current VAPOR dump",required=True)
 parser.add_argument("--map",help="the file for translating the Metabolizer status",required=True)
 parser.add_argument("--answer",help="Answer file, maybe. ")
+parser.add_argument("--trans",help="transcript to name mapping")
 args = parser.parse_args()
 oldfi = args.vapor
 newfi = args.map
 ansfi = args.answer
+trnsfi = args.trans
+
 
 mapfi = open(newfi,'r')
 maps = csv.DictReader(mapfi,delimiter="\t")
@@ -20,8 +23,9 @@ answer = csv.DictReader(ansfi,delimiter="\t")
 vaporfi =  open(oldfi,'r')
 vapors =  csv.DictReader(vaporfi)
 csvfile = oldfi + ".new.csv"
-newvapor = csv.writer(open(csvfile, 'w'),  delimiter=',')
-
+newvapor = csv.writer(open(csvfile, 'w'),  delimiter=',',quoting=csv.QUOTE_ALL)
+transf = open(trnsfi,'r')
+trans = csv.DictReader(transf,delimiter="\t")
 
 #parse results in a map or dict, or what??
 #vapors: is in a dict.
@@ -34,36 +38,38 @@ newvapor = csv.writer(open(csvfile, 'w'),  delimiter=',')
 ####    if good, do nothing.
 ####    if not good or blank, update file.
 ####        emit output <--> the update that was made
-#
-
 
 #-------------------------------------here by DEFSgONS!!----------------------------------*
 
 def getVal(desiredval,answer):#the line being needed to map == desiredval, the full answerfile == answer
-
     variant_uuid = None
 
     def __match_rs__(rs_targ):#get correct rsid
-
         ansfi.seek(0)
 
-        def __matchalt__(targ,ansl,nucl,full_answer,desval):#get the right hgvs nucleotide == desired nucleotides
+        def __split__(nucl):#decide if snp or del
+            if len(nucl) == 2:
+                return(list(nucl))
 
-            def __split__(nucl):#decide if snp or del
-                if len(nucl) == 2:
-                    return(list(nucl))
+            else:
+                alts = nucl
 
-                else:
+                if "/" in nucl:##find the right splitters.
                     alts = nucl.split("/")#here, I am getting Del worked out
 
-                    if 'D' in alts[0]:
-                        return(alts[0].lower(),alts[1].lower())
+                elif "-" in nucl:
+                    alts = nucl.split("-")#suggested here
 
-                    elif 'D' in alts[1]:
-                        return(alts[0],alts[1].lower())
+                if 'D' in alts[0]:
+                    return(alts[0].lower(),alts[1].lower())
 
-                    else:
-                        return(alts[0],alts[1])
+                elif 'D' in alts[1]:
+                    return(alts[0],alts[1].lower())
+
+                else:
+                    return(alts[0],alts[1])
+
+        def __matchalt__(targ,ansl,nucl,full_answer,desval):#get the right hgvs nucleotide == desired nucleotides
 
             def __choosehomo_or_wt__(targ_refalt,ansl,full_answer):#targrefalt, which ansl is it?
                                                                    #if ref matches targ[0] and targ[1] its WT+
@@ -71,14 +77,16 @@ def getVal(desiredval,answer):#the line being needed to map == desiredval, the f
                     annuc = an.split('-')                          #if ref matches none, its homovar
                     return(annuc[1:])
 
-                if __getansnuc__(ansl)[0] != nucl[0] and  __getansnuc__(ansl)[1] == nucl[1]:
+                if __getansnuc__(ansl)[0] != __split__(nucl)[0] and  __getansnuc__(ansl)[1] == __split__(nucl)[1]:#ref ne inc1 and both nucs are same
                     return full_answer['homohgvs']
 
-                elif __getansnuc__(ansl)[0] == nucl[0] and  __getansnuc__(ansl)[1] == nucl[1]:
+                elif __getansnuc__(ansl)[0] == __split__(nucl)[0] and  __getansnuc__(ansl)[1] == __split__(nucl)[1]:#this is the ref alt case, ref = nuc1 and alt = nuc2
                     return full_answer['hethgvs']
 
+                elif __getansnuc__(ansl)[0] == __split__(nucl)[1] and  __getansnuc__(ansl)[1] == __split__(nucl)[0]:#this is the alt ref case,
+                    return full_answer['hethgvs']
 
-                elif __getansnuc__(ansl)[0] == nucl[0] and  __getansnuc__(ansl)[0] == nucl[1]:
+                elif __getansnuc__(ansl)[0] == __split__(nucl)[0] and  __getansnuc__(ansl)[0] == __split__(nucl)[1]:
                     return full_answer['wthgvs']
 
 
@@ -86,16 +94,13 @@ def getVal(desiredval,answer):#the line being needed to map == desiredval, the f
             correct_hgvs = __choosehomo_or_wt__(targ_refalt,ansl,full_answer)#get the correct hgvs in the ansl
 
             def __fixhgvs__(hgvs,dval,nuc,desval):#straight split and reverse??yes.
-
                 try:
                     hgvslist = hgvs.split(':')
                     newhgvs = ':'.join(hgvslist[1:]) + "||" + hgvslist[0]
-
                     return newhgvs
 
                 except AttributeError:
                     print(str(dval) + "\t" + str(hgvs) + "\t" + str(nuc) + "\tUNABLE TO FIND RSID MAP" + "\t" + str(desval))
-
 
             fixedhgvs = __fixhgvs__(correct_hgvs,targ,nucl,desval)
             return(fixedhgvs)
@@ -104,49 +109,152 @@ def getVal(desiredval,answer):#the line being needed to map == desiredval, the f
 
         for ansline in answer:#construct final returns two values as there are two matches, unfound because there is a mismatch,
                               #NEXT:: make specific for the alt value needed for multi alts, and reverse alts
-            if rs_targ['rsID'] in ansline['rsid']:
+            def __choose_correctans__(rsinc,rsans):#return the correct ALT
+                                                   #find if the alt matches alt.
+                altval = "NoneText"                #ref alt ONLY for the correct answer line
+                def __get_REF_rsans__(ans):
+                    refgetter = ans.split('-')
+
+                    return refgetter[1]
+
+                inc_nuc = __split__(rsinc)
+                ref =__get_REF_rsans__(rsans)
+
+                if ref in rsinc and inc_nuc[0] != inc_nuc[1]:#this is to test if this is het
+                    #print("IN THE HETTER  " + str(ref) + "\t" + str(inc_nuc))
+
+                    if ref == inc_nuc[0]:
+                        altval = inc_nuc[1]
+
+                    else:
+                        altval = inc_nuc[0]
+
+                elif ref in rsinc and inc_nuc[0] != inc_nuc[1] and ref == inc_nuc[1]:#ref is inc_nuc[1]
+                    altval = inc_nuc[1]
+
+                elif ref in rsinc and inc_nuc[0] != inc_nuc[1] and ref == inc_nuc[1]:#ref is inc_nuc[0]
+                    altval = inc_nuc[0]
+
+                elif ref not in rsinc:
+                    altval = inc_nuc[0]
+
+                else:
+                    altval = ref
+
+                return altval
+
+            if rs_targ['rsID'] in ansline['rsid'] and __choose_correctans__(rs_targ['Diplotypes'],ansline['rsid']) in ansline['rsid']:#here, filter for correct
+
                 correctcdot = __matchalt__(rs_targ,ansline['rsid'],desiredval['Diplotypes'],ansline,desiredval)
                 #print(str(correctcdot) + " should be ok for correct dot")
                 return correctcdot
+
+    def __get_trans__(sym,trfi):#get trans, with read to new dict as sym for key
+        trfi.seek(0)
+
+        def __createdict__(trf):
+            trnsdict = {}
+            for line in trf:
+                trnsdict[line['sym']] = line['trans']
+            return trnsdict
+
+        transcriptid = __createdict__(trans)[sym]
+        return transcriptid
 
     if desiredval['rsID'] is not "":#match RSID to cdot, then to correct FULL HGVS.
         #variant_uuid = __match_rs__(desiredval['rsID'])#change, there are multiple
         variant_uuid = __match_rs__(desiredval)#change, there are multiple
 
+    elif desiredval['Diplotypes'] is not "":#give justhe NM_ID for haplotypes here. also, set the NAT2. becareful of *N/*M appearing as *M/*N
+        variant_uuid =  desiredval['Diplotypes'] +"||"+ __get_trans__(desiredval['Gene'],transf)
 
-    else:#give justhe NM_ID for haplotypes here. also, set the NAT2. becareful of *N/*M appearing as *M/*N
-        #print(str(desiredval) + ' pass through')
-        variant_uuid = 'pass through'
-
+    else:#assuming its just the naked genetype
+        variant_uuid =  "ALL||" + __get_trans__(desiredval['Gene'],transf)
 
     return variant_uuid
 
 
-
-
+def printvar(met,vape,outfile):
+    outfile.writerow([vape["Write Up ID"],vape['UUID'],vape["Drug Name"],met])
 
 #####----------------MAIN--------------####      #####----------------MAIN--------------####
 
-
 mapdat = {}
-
 
 #construct map for replacement table
 for mapln in maps:
 
-    print("INCOMING MAP LINE \t" + str(mapln))
     valid_uuid = getVal(mapln,answer)
-    print(str(valid_uuid) + "\tASSIGNED UUID")
+    mapdat[valid_uuid] = mapln['Metabolizer Status']
+
+#map constructed, proceed.
+
+
+newvapor.writerow(["Write Up ID","UUID","Drug Name","Metabolizer Status"])
+
+for vape in vapors:# OK, well, get the uuid, then, decide if it is NAT2, or not NAT2.
+                   #then decide if it is SNPs or not SNPs finally, get the HAPs.
+                   #access the all if needed, but just make sure the Metabolizer Status is filled in.
+                   #
+                   #only write out the things that are in:
+                   #    # error
+                        #SNPs
+                        #HAP2 snps
+
+    def __getUUID__(vp):
+        ruuid = vp['UUID'].split('+')[0]
+        return(ruuid)
+
+    vuuid = __getUUID__(vape)#
+
+    if 'NM_000015' in vuuid and "*" in vuuid:
+
+        print("GET THE NAT2 SHIT TOGETHER")
+
+    elif 'c.' in vuuid:
+
+        try:
+            met_status = mapdat[vuuid]#mapdat is the met status from the file.
+            printvar(met_status,vape,newvapor)
+
+        except KeyError:
+            print("Unable to find Metabolizer Status for\t"+vuuid+"\t"+str(vape['Drug Name'])+"\t"+str(vape['Evidence'])+"\t"+str(vape['Recommendation'])+"\t"+str(vape['Implication'])+"\t"+str(vape['Severity'])+"\t"+str(vape['Archived Implication Field']))
+
+    else:
+        print(the fuck is this )
 
 
 
-#olddat = {}#stores the old calls. chr:pos
-
-#for oln in olds:
-    #print str(getVal(oln,'chrN',transdat))
 
 
-#print "position\tnewcall\toldcall"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
