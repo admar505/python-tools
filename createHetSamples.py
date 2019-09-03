e041f917b5a324061d3cf9408e1ae8f5c0b7625c
Diego Martinez <diego@veritasgenetics.com>
2019-09-03 19:41:32 +0000

    Synchronized the het homo wt GT output with url

--- createHetSamples.py
+++ createHetSamples.py
@@ -8,7 +8,7 @@ import csv
 #then, then make a map to of res. read through the vcf, and add to reslines. If res in vcf, then
 #add score and rsid to EFF_EFFECT. if not, then grab line from big vcf, and process like olden times as if new. call recover.RESULTS.txt
 #NEW VERSION: go through CSV, pull out the RESults file, if not in, then recreate from vcf.
-parser = argparse.ArgumentParser(description="replace files with rsid driven nodes and hgvs designations")
+parser = argparse.ArgumentParser(description="replace files with rsid driven nodes and hgvs designations, if the variant DOESNT have rsid, then remove from file thanks")
 parser.add_argument("--res",help="COMPLETE.txt or whole genome RESULTS.txt file")
 parser.add_argument("--bed",help="the answer.bed, but with the rsid-REF-ALT converted to rsid")
 parser.add_argument("--url",help="the url to use")
@@ -23,6 +23,30 @@ res = vgr.Reader(open(resfi,'r'))
 #trns = csv.DictReader(open(trnsfi,'r'),delimiter='\t')
 newfile = vgr.Writer(open(hg + '.new.' + resfi,"w"))
 
+def correctGT(req_gt,bedline):
+    genotype = None
+
+    def __getALT__(rsid,allele):
+
+        alt = None
+        fgt = re.search('rs\w+\-(\w+)\-(\w+)',rsid)
+
+        if "hom" in allele:
+            print("captured")
+            alt = fgt.group(2) + fgt.group(2)
+
+        elif "wt" in allele:
+            alt = fgt.group(1) + fgt.group(1)
+
+        else:
+            alt = ''.join(sorted(''.join([fgt.group(1),fgt.group(2)])))
+
+        return(alt)
+    #------#
+    genotype = __getALT__(bedline['rsid'],req_gt)
+
+    return genotype
+
 
 
 def rsidline(trans_file,rsid):#returns the correct var line, but, I have the special line.
@@ -31,20 +55,24 @@ def rsidline(trans_file,rsid):#returns the correct var line, but, I have the spe
     trans_file.seek(0)
 
     for fline in trans_fi:
-
-        if str(fline['rsid']) == str(rsid):
+        #if str(fline['rsid']) == str(rsid):
+        if str(rsid) in str(fline['rsid']):
             rets = fline
 
     return rets
 
 
-
-
 for line in res:
-    rsold = rsidline(trnsfi,line.INFO['RSID'])
-    line.INFO['EFF_HGVS']  = rsold[hg]
-    line.INFO['VAPOR_URL'] = rsold[urls]
+    if line.ALT != "NULL":
+        rsold = rsidline(trnsfi,line.INFO['RSID'])
+        line.INFO['EFF_HGVS']  = rsold[hg]
+        line.INFO['VAPOR_URL'] = rsold[urls]
+        line.INFO['FBGenoType'] = correctGT(hg,rsold)
 
-    newfile.write_record(line)
+        newfile.write_record(line)
+
+    else:
+
+        newfile.write_record(line)
 
 
