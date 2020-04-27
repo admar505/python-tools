0354d7ff84722caba07ab1eb12ea75bf41ddeafa
Diego Martinez <diego@veritasgenetics.com>
2020-04-27 16:29:04 -0400

    primer mapper is now able to print out multi hits.

--- parsePrimerBlastN.py
+++ parsePrimerBlastN.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-
 import argparse
 import string
 import sys
@@ -29,10 +28,62 @@ pmapfi = open(args.pr,"r")
 
 ##+++++++++++++++++++DEFS+++++++++++++++##
 
-def getLongest(pr,st,sp):
+def getLongest(qry, prmp,st,sp):#$qryname, primer dict, start and $ stop on query, calculate proportion of match
 
-    return int(10)
+    length = float((int(sp) - int(st))/prmp[qry])
 
+    return float(length)
+
+
+
+def addNew(best,blast,new,pmap):#besthit_dict, blastline if new is True, it will add to the hash, if False, it will just replace the values there.
+
+
+    if new == True:
+        best[blast['queryacc.ver']] = {}
+        best[blast['queryacc.ver']][blast['subjectacc.ver']] = {}
+
+    best[blast['queryacc.ver']][blast['subjectacc.ver']] = {"longesthit":getLongest(blast['queryacc.ver'],pmap,blast['q.start'],blast['q.end']), "gaps":int(blast['mismatches'] + blast['gapopens'])}
+
+
+def queryDict(queryd):
+
+    for key in queryd:
+        print(queryd[key])
+
+
+def Printer(line):
+
+    print(';'.join(line))
+
+
+
+
+def Collapser(bt):
+
+    for res in bt:
+
+        prnstore = []
+        prnstore.append(res)
+
+        for qrres in bt[res]:
+            prnstore.append( qrres +  ";" +  bt[res][qrres])
+
+        Printer(prnstore)
+
+
+def findPrimerMatches(bst):#will flip the value, and report the primer hits and what the scores are for subject
+
+    sbjhit = {}
+
+    for qr in bst:
+        for subject in bst[qr]:
+            sbjhit[subject] = {}
+            sbjhit[subject][qr] = str(bst[qr][subject]['longesthit']) + ":" + str(bst[qr][subject]['gaps'])
+
+
+
+    Collapser(sbjhit)
 
 
 ##===================MAIN===============##
@@ -41,37 +92,46 @@ pmap = {}#contains the primer lengths.
 best = {}#primer is key, value is hit. for every key val, store length of primer hit, and mismatches + gaps.
 
 
-for primer in pmap:
+for primer in pmapfi:
     pkey = primer.split("\t")
-
-    pmap[pkey[0]] = pkey[1]
-
+    pmap[pkey[0]] = int(pkey[1])
 
 
 
 for blast in blncsv:
-    print(blast)
+    #print(blast)
+
     if blast['queryacc.ver'] in best:
 
-        ##translate subject accesion to species, and dont care about anything less,
+        ##maybe translate subject accesion to species, and dont care about anything less,
+        #queryDict(best)
 
-        if blast['subjectacc.ver'] in best[blast['queryacc.ver']]:
 
-            print(blast['subjectacc.ver'] )
+        if blast['subjectacc.ver'] in best[blast['queryacc.ver']]: #query subj value exist
+
+            #print(best[blast['queryacc.ver']] )
+            #the alg:
+            best_long = best[blast['queryacc.ver']][blast['subjectacc.ver']]['longesthit']
+            new_long = getLongest(blast['queryacc.ver'],pmap,blast['q.start'],blast['q.end'])
+            best_gap = best[blast['queryacc.ver']][blast['subjectacc.ver']]['gaps']
+            new_gap = int(blast['mismatches'] + blast['gapopens'])
+
+            if new_long > best_long and best_gap <= new_gap:
+                addNew(best,blast,False,pmap)
+
+                #I may add a condition.
+
+
 
 
     else:#adding new query, if no query add all in.
 
-        best[blast['queryacc.ver']] = {}
-        best[blast['queryacc.ver']][blast['subjectacc.ver']] = {}
-        best[blast['queryacc.ver']][blast['subjectacc.ver']] = {"longesthit": getLongest(pmap,blast['q.start'],blast['q.end'])}
-        best[blast['queryacc.ver']][blast['subjectacc.ver']] = {"gaps":blast['mismatches'] + blast['gapopens']}
+        addNew(best,blast,True,pmap)
 
 
+findPrimerMatches(best)
 
-
-
-#print out blast
+#print out blast, but before I think I can collect, for each subject, the queries it gets.
 
 
 
