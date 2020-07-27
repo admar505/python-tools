8a4478c3514097cc2c351bf12c8fb928a0bd806a
Diego Martinez <admar505@gmail.com>
2020-07-27 16:37:25 -0400

    reorg hmmtools to allow for accession search

--- hmmTools.py
+++ hmmTools.py
@@ -5,11 +5,15 @@ import random
 parser = argparse.ArgumentParser(description="manipulate HMM records and files")
 parser.add_argument("--hmm",help="HMM file",required=True)
 parser.add_argument("--chnk",help="Chunk size, number of records per file",required=True)
+parser.add_argument("--name",help="if used, will output file only if it has this name, can be used multiple times.",action='append')
 
 #number of records per
 args = parser.parse_args()
 hmmfi = args.hmm
 chnk = args.chnk
+names = args.name
+
+
 
 #psuedo
 #
@@ -36,12 +40,41 @@ def outname(filename,currentcount ):
     return outfiname
 
 
+def acccheck(names2find,filelines):
+    found = False
+
+    for names in names2find:
+        if names in filelines:
+            found = True
+
+
+    return found
+
+
+def recprint(outfi,filelines):
+    
+
+        outfi.close()
+
+        outfi = open(outname(hmmfi,ficount),"w")    #start new OUT
+        ficount = ficount + 1
+
+        outfi.write(filelines + "\n")        
+
+
+
 ###main###
 
+#reconf for one record storage at a time.
+
+
+#####initializers.
+
 ficount = 1
+outfi = open(outname(hmmfi,rct),"w") #initialize with one.
+filelines = []                       #this will hold the files. and dump as needed.
+accession = None                     
 
-
-outfi = open(outname(hmmfi,rct),"w")
 
 
 for line in hmm_orig:
@@ -49,23 +82,36 @@ for line in hmm_orig:
     #print(str(rct))
 
     #check if file needs to be updated
-    if int(rct) >= int(chnk):
-        rct = 0
-        outfi.close()
-        outfi = open(outname(hmmfi,ficount),"w")
-        ficount = ficount + 1
+    if int(rct) >= int(chnk):   #start new file.
+        rct = 0                 #RESET current file count
+       
+
+        recprint()
 
 
-    if line == "//":#increase count for records
+        filelines = []          #clear holder
+       
+
+
+
+        filelines.append(line  + "\n")
+
+    elif line == "//":#increase count for records
         rct = rct + 1
+        accession = None
+    #added to allow accession ids or name checks.
+        
+        if names is not None:
+           
+            if accessioncheck() is True:
+                printrec
+        else #this means we are NOT in a name control issue
 
-    outfi.write(line  + "\n")
 
 
+    if:#just read normal
 
-
-
-
+        filelines.append(line + "\n")
 
 
 
