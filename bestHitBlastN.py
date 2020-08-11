2231536ea380dccbecc42dca832242b2c0bfcb3c
Diego Martinez <admar505@gmail.com>
2020-08-11 12:52:32 -0400

    blast added back

--- bestHitBlastN.py
+++ bestHitBlastN.py
@@ -5,6 +5,8 @@ import string
 import sys
 import subprocess
 import csv
+from collections import defaultdict
+from operator import itemgetter,attrgetter
 
 parser = argparse.ArgumentParser(description='best reciprocal hit to all the hits, with annotations.')
 parser.add_argument("--ann", help="accesion to id lines, so ",required=True)
@@ -31,32 +33,55 @@ annfi = open(args.ann,"r")
 
 ##+++++++++++++++++++DEFS+++++++++++++++##
 
-def getLongest(bln,st,sp):#$qryname, primer dict, start and $ stop on query, calculate proportion of match
+def getLongest(bln):#$qryname, primer dict, start and $ stop on query, calculate proportion of match
 
-    length = float((int(sp) - int(st))/[qry])
-    #print(str(length) + "\t" + str(sp) +"\t" + str(st))
+    length = int(bln['alignmentlength']) - int(bln['mismatches']) - int(bln['gapopens'])
+
     return float(length)
 
+def tree():return defaultdict(tree)
+
+def addNew(best,blast,newqry,newsbjct):#edge, it will add
+                                            #update: ADD BOTH DIRECTIONS.
+                                            #newqry: to add in new first edge
+                                            #newsubj:to add in new terminal edge.
+                                            #in each, test the opposite.
+    #print(str(blast['queryacc.ver']) + "\taddNew line 47")
+    lhit = getLongest(blast)
+
+    if newqry == True and newsbjct == False:#newedge add
+
+        if blast['queryacc.ver'] not in best:
+            best[blast['queryacc.ver']] = {}
+
+        if blast['subjectacc.ver']  not in best:
+            best[blast['subjectacc.ver']] = {}                 #this will be confusing because of the flipped edges, there
+                                                                   #is lots of testing are retesting if thinkgs exist.
+
+        #critical error, I have to see things multiple times in order to report, if its once , will never catch it. so I am leaving out the small stuff here.
 
 
-def addNew(best,blast,newqry,newsbjct,qry):#besthit_dict, blastline if new is True, it will add to the hash, if False, it will just replace the values there. this is contig blast agnostic, pass it and it will add.
-                                            #newqry: to add in only new query.
-                                            #newsubj:to add in only new sbjct.
-                                            #need to adjust to know which to add, the query or subj as key.
-    print(blast)
-    if newqry == True:
-        best[blast['queryacc.ver']] = {}
+        best[blast['subjectacc.ver']][blast['queryacc.ver']] = {"longesthit":lhit, "score":float(blast['bitscore'])}
+        best[blast['queryacc.ver']][blast['subjectacc.ver']] = {"longesthit":lhit, "score":float(blast['bitscore'])}
 
+    if newsbjct == True and newqry == False:#adding in terminal edge node.
 
-    if newsbjct == True:
-        best[blast['queryacc.ver']][blast['subjectacc.ver']] = {}
+        if blast['queryacc.ver'] not  in best[blast['subjectacc.ver']]:
+            best[blast['subjectacc.ver']][blast['queryacc.ver']] = {"longesthit":lhit, "score":float(blast['bitscore'])}
 
+        if blast['subjectacc.ver'] not in best[blast['queryacc.ver']]:
+            best[blast['queryacc.ver']][blast['subjectacc.ver']] = {"longesthit":lhit, "score":float(blast['bitscore'])}
+
+        #best[blast['queryacc.ver']][blast['subjectacc.ver']] = {"longesthit":lhit, "score":blast['bitscore']}
+        #best[blast['subjectacc.ver']][blast['queryacc.ver']] = {"longesthit":lhit, "score":blast['bitscore']}
 
     #if both subj and qry is already in, just add the new values.
-    lhit = getLongest(blast['queryacc.ver'],pmap,blast['q.start'],blast['q.end'])
-    gap = int(blast['mismatches']) + int(blast['gapopens'])
 
-    best[blast['queryacc.ver']][blast['subjectacc.ver']] = {"longesthit":lhit, "gaps":gap}
+
+    if newqry == False and newsbjct == False:#this is if the new vals are better than what is already there.
+
+        best[blast['queryacc.ver']][blast['subjectacc.ver']] = {"longesthit":lhit, "score":float(blast['bitscore'])}
+        best[blast['subjectacc.ver']][blast['queryacc.ver']] = {"longesthit":lhit, "score":float(blast['bitscore'])}
 
     #print(blast['subjectacc.ver'] + "\t" +str(lhit)  + "\t" + str(gap))
 
@@ -64,13 +89,18 @@ def addNew(best,blast,newqry,newsbjct,qry):#besthit_dict, blastline if new is Tr
 def queryDict(queryd):
 
     for key in queryd:
-        print(key + "+" +  str(queryd[key]))
+        print(key + "+DICTIONARY TEST" +  str(queryd[key]))
 
 
-def Printer(line):
+def Printer(cntg, hit, blastvals,pmp):
 
-    ct = len(line) - 1
-    print(str(ct) + "\t" + ';'.join(line))
+
+    name = pmp[hit]
+
+    scr    = blastvals[cntg][hit]['score']
+    length = blastvals[cntg][hit]['longesthit']
+
+    print("FINAL VALUE\t\t" + str(cntg) + "\t" + str(hit) + "\t" + str(scr) + "\t" +  str(length) +"\t" +name)
 
 
 def Collapser(bt):
@@ -87,93 +117,127 @@ def Collapser(bt):
         #print(str(res) + "\t"  + str(prnstore))
         Printer(prnstore)
 
+def scoreSorted(grph,start):#return best based on score.
+                            #how to? get all scores, and sort, and then find the hit that score is too.
+    bestedge = ""
+    scores = []
+    def __sorter__(gr,st,score):
 
-def findRecips(bst):#will flip the value, and report the  hits and what the scores are for subject
+        for hit in gr[st]:
 
-    sbjhit = {}
+            if float(gr[st][hit]['score']) == score:
+                return(hit)
+
+    for hit in grph[start].keys():
+
+        scores.append(float(grph[start][hit]['score']))
+
+    bestedges = sorted(scores,reverse = True)[0]
+    besthit = __sorter__(grph,start,bestedges)#for several hours I just missed the "s" at the end.
+
+    return(besthit)
+
+
+def findRecips(cntg,bst,titles):#cntg is the NODE, bst is edge graph will flip the value, and report the  hits and what the scores are for subject
+    print("finding matches")
+
+
 
     #hmmm, run through, and collect all subjects? ZZ
 
-    for qr in bst:
+    def __getbestnode4edge__(graph,nde):#graph is the blast list, node is the query
+        bestcontig = False
+
+        if nde in graph and bool(graph[nde]) is True:
+
+            bestcontig = scoreSorted(graph,nde)
+           # print("     get best node 4 edge " + str(bestcontig))
+        return(bestcontig)
+
+    for node  in cntg:
+        #print(str(node) + "\tline 111")
         #how to flip??#oh, subject needs to go in once, once only. so check and see if it needs to be added.
+        try:
 
-        for subject in bst[qr]:
-            #print(subject + "\t" + qr)
+            if node in bst and bool(bst[node]) is True:#filterout the
+                bestedge = scoreSorted(bst,node)#get the best match, by values. but which value?
+                #print(str(node) +"\t" + str(bst[node])   +"\tBESTEDGE\t" +str(bestedge) )
+                bestnode = __getbestnode4edge__(bst,bestedge)#THE BEST TERMINAL NODE!!!
 
-            if subject not in  sbjhit:
-                sbjhit[subject] = {}
-
-            sbjhit[subject][qr] = {}
-            sbjhit[subject][qr] = str(bst[qr][subject]['longesthit']) + ":" + str(bst[qr][subject]['gaps'])
+                #print(str(node) + "\tline 112\t" + str(bestedge) +"\t" + str(bestnode))
+                if node == bestnode:#THE MAGIC!!! DOES THE NODE match the termimal edges best NODE????
+                    Printer(node,bestedge,bst,titles)
 
 
+        except IndexError:
 
-    #queryDict(sbjhit)
-    Collapser(sbjhit)
+            print(str(node) + "\tEDGE NOT FOUND\t"  + str(bst[node]))
+
+
+    #queryDict(bst)
+    #Collapser(bjhit)
 
 
 ##===================MAIN===============##
 #get the node into a dict.
 pmap = {}#contains the annotation headers to sew back on..
-bestqr = {}#CONTIG is is key, value is hit. for every key val, store length of  hit,score, and mismatches + gaps.
-           #for length, subtract gaps and mismatches, so, make it a qualified hit.
-           #may need to adjust to allow for gaps. ways to gap but break? alse perhaps HSP merging?
-bestdb = {}#db is key, value is contig/query. for every key val, store length of hit, -  mismatches + gaps.
+bestqr = {}#contig IS SUBJ is key, NODES value is key, STORE ALL CONTIGS, this is NODE list
 
+bestdb = tree()#db and (visa versa  ) EDGES    is key, value is contig/query. for every key val, store length of hit, -  mismatches + gaps.
+           #   EDGES!! use as edges and do the best hit versions
 
 for annline in annfi:
-    annkey = annline.split("\s")
-    pmap[annkey[0]] = annline
+    annkey = annline.split(" ")
+    pmap[annkey[0]] = annline.strip()
 
 #parse blast file, sort into different buckets.
 #so, in short, load, but only take if metrics are better.
 
-
 for blast in blncsv:
     #print(blast)
 
-    if blast['queryacc.ver'] in bestqr:#the contig has been seen as key. if not add.
+    bestqr[blast['subjectacc.ver']] = blast['subjectacc.ver']
+
+
+
+    if blast['subjectacc.ver'] in bestdb and blast['queryacc.ver'] in bestdb:#the contig has been seen as key. if not add.
 
         ##maybe translate subject accesion to species, and dont care about anything less,
         ##ah, so this is going to be harder. can I assume if the contig/query has contains the pair,
         ##then the dbhash does? no, I cannot. I think. just test?
 
-        #print(best[blast['queryacc.ver']])
-
-
-        if blast['subjectacc.ver'] in bestdb[blast['queryacc.ver']] and b:#query subj value already exist,
+        if blast['subjectacc.ver'] in bestdb[blast['queryacc.ver']] and  blast['queryacc.ver'] in bestdb[blast['subjectacc.ver']]:#query subj value already exist,
+        #if blast['queryacc.ver'] in bestdb[blast['subjectacc.ver']]:#query subj value already exist,
                                                                   #so just test if new values are better.
                                                                   #
-            #print(blast['queryacc.ver'] + "\t" + str(best[blast['queryacc.ver']]) )
-            #the alg:
+
             best_long = bestdb[blast['queryacc.ver']][blast['subjectacc.ver']]['longesthit']
-            new_long = getLongest(blast['queryacc.ver'],pmap,blast['q.start'],blast['q.end'])
-            best_gap = best[blast['queryacc.ver']][blast['subjectacc.ver']]['gaps']
-            new_gap = int(blast['mismatches']) + int(blast['gapopens'])
-            best_score = blast['bitscore']
+            #print(str(best_long) + "     BEST LONGEST")
+            new_long = getLongest(blast)
+            best_score = bestdb[blast['queryacc.ver']][blast['subjectacc.ver']]['score']
+            new_score = float(blast['bitscore'])
 
             #print(blast['subjectacc.ver']  +"\t"+ blast['queryacc.ver'] + "\t" + str(new_long) +"\t "+ str(new_gap))
 
+            score_threshold = best_score*(1-args.better)
 
-            if new_long >= best_long and best_gap >  new_gap:
-                addNew(best,blast,False,False)#newqry, newsbjct are bools
+            if new_long > best_long and float(new_score) >  float(score_threshold):#THIS PICKS THE BEST,
+                addNew(bestdb,blast,False,False)#newqry, newsbjct are bools
 
                 #I may add a condition.
 
+
         else:#ok, try new logic for adding only the query,
              # I think I might be resetting if I load in new query if I load both
-            addNew(best,blast,False,True)#newqry, newsbjct are bools
+            addNew(bestdb,blast,False,True)#newqry, newsbjct are bools
 
 
     else:#adding new query, if no query add all in.
-        addNew(bestqr,blast,True,True)#newqry, newsbjct are bools
-
+        addNew(bestdb,blast,True,False)#newqry, newsbjct are bools and just mean
 
 findRecips(bestqr,bestdb,pmap)
 
 #print out blast, but before I think I can collect, for each subject, the queries it gets.
-
-
 #queryDict(best)
 
 
