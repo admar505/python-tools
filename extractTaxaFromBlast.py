f8611ac2cc8716c9c7b85183a315364c4edce7d2
Diego Martinez <admar505@gmail.com>
2020-10-01 13:48:15 -0400

    final testing phase

--- extractTaxaFromBlast.py
+++ extractTaxaFromBlast.py
@@ -27,21 +27,40 @@ def loadIDs(taxastructure,needDict):#NCBI taxaids, dictionary to store
     
     for branches in taxastructure:
         needDict[branches] = branches
+        
 
-def getGIs(gifi,gidict,txidDict):#proteinFile, 
+        try:  #necessary in case there are no taxa descendants. should call em F2s or something
+
+            for leaves in taxastructure[int(branches)]:
+                needDict[leaves] = leaves
+        
+        except IndexError:
+            
+            continue
+
+def getGIs(gifi,gidict,txidDict):#proteinFile, storing of GIs that are important, taxalist to use for that. 
 
     for gline in gifi:
         cols = gline.split("\t")
-
-        if cols[2] in txidDict:
-
+         
+        if str(cols[2].strip()) in str(txidDict.keys()):
             gidict[cols[3].strip()] = cols[2]
 
 def giYoink(g1):#just deflines.
-    
     gen1 = g1.split("|")[1]
-
+    
     return(gen1)
+
+
+def keepGI(gb1,gb2,gdct):#twoGIs,#one dictionary.
+    
+    should_keep = False
+
+    if gb1  in gdct or  gb2  in gdct:
+        should_keep =  True
+
+    return(should_keep)
+
 
 ###---------DaTA handlers and dictionary space----------###
 
@@ -51,8 +70,6 @@ gis2save = {}       #Has the GI ids that will be ok to keep.
 ncbi = ntt.NcbiTaxonomyTree(nodes_filename=args.nodes, names_filename=args.names)
 
 ###main---mainly in Maine-------###whydoIthinkIamfunny
-
-print(taxids)
 
 for tx in taxids:
 
@@ -66,18 +83,26 @@ for tx in taxids:
 
 #eesh. now load the monster of the giTaxadeal.
 
-getGIs(prot2idfi,gis2save,treeIDs2names)
+getGIs(prot2idfi,gis2save,treeIDs2names)#gi_lst,#gisthatarekosher,#
+
+
 
 #ok. parse it in.
 
 for blline in blastfi:
-
-    bols = blline.split("\t")
     
-    (gi1,gi2) = giYoink(bols[1],bols[0])
-
-    if 
-    
+    if "#"  not in blline:
+        bols = blline.split("\t")#grab some GIs.
+        
+        gi1 = giYoink(bols[0])
+        gi2 = giYoink(bols[1])  
+        
+        if gi1 != gi2:  #filter self-hits out, and begin lookup for taxa.
+                        #get the gis that are particular.
+            keep = keepGI(gi1,gi2,gis2save)
+            
+            if keep == True:
+                print(blline.strip())
 
 
 
