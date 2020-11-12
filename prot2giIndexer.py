c6d05671eac263860709582aef96e965950a4116
Diego Martinez <admar505@gmail.com>
2020-11-12 14:41:31 -0500

    testing first pass of pickler. current issue is the binary files are massive.

--- prot2giIndexer.py
+++ prot2giIndexer.py
@@ -1,101 +1,79 @@
 #!/usr/bin/env python3
 import sys,os,re,fileinput,argparse
 sys.path.append('/home/nucleo/lib/NCBI_taxonomy_tree')
-import ncbiTaxonomyTree as ntt
 import gzip
-import whoosh
-from whoosh.fields import Schema, TEXT, KEYWORD, ID, STORED
-import whoosh.index as index
 import os.path
-from whoosh.qparser import QueryParser
+import pickle
+import _pickle as cp
 
-parser = argparse.ArgumentParser(description="create a whoosh index for prot.accession2taxid.gz ")
+parser = argparse.ArgumentParser(description="create a binary index for prot.accession2taxid.gz ")
 parser.add_argument("--prot2id",help="the prot.accession2taxid.gz ",required=True)
-parser.add_argument("--blp",help="The Blast output, fmt7 ")
-
-
-
-blastout = None
 
 args = parser.parse_args()
 
-
-if args.blp is not None:
-    blastout = open(args.blp,"r")
-
-
 #prot2idfi = gzip.open(args.prot2id,"rt")
-prot2idfi =open(args.prot2id,"r")
+prot2idfi = gzip.open(args.prot2id,"rt")
 
 ####two passes, first, collect ids. second, add on. 
-##get index for whoosh to work.
+##get index for me stuff to work.
 ##
 ###---------------defgers oh my! --------------###
 
 
 ###---------DaTA handlers and dictionary space----------###
 
-gi_schema = Schema(
-                content=TEXT(stored=True),
-                title=TEXT(stored=True),
-                body=TEXT(stored=True)
-                #accession=ID(stored=False),
-                #accessionversion=ID(stored=False),
-                #taxid=ID(stored=True),
-                #gi=ID(stored=True)
-                )
-
-acc_schema = Schema(
-                accessionversion=ID(stored=True),
-                taxid=ID(stored=True))
+gi = []         # for genbank accession.
+taxa = []       # for tax id.
+acc = []        # for storing the accessions.
 
 
 
 
 ###main---mainly in Maine-------###whydoIthinkIamfunny
+##
 
-#ok, it has to have an index path, kinda silly. anyway
-if not os.path.exists("indexdir"):
-    os.mkdir("indexdir")
-
-#create storage or index object
-gi_idx = index.create_in("indexdir", schema=gi_schema, indexname="gi.p2id.idx")
-#gi_idx = index.open_dir("indexdir",indexname="gi.p2id.idx")
-#acc_idx = index.create_in("indexdir", schema=acc_schema, indexname="acc.p2id.idx")
-
-#open writer, add the document
-
-gi_write = gi_idx.writer()
-
-#gi_write.add_document(title=u"Gi.index",content=prot2idfi)
-gi_write.add_document(title=u"test.prots.acc2tid.lst",content="test.prots.acc2tid.lst")
-gi_write.commit()
+##might need to do this with islice.
+for pl in prot2idfi:
+    
+    cols = pl.split("\t")
+    
+    gi.append(cols[3])
+    taxa.append(cols[2])
+    acc.append(cols[1])
 
 
-#ok, lets see if it worked it seems to have taken.
+prot2idfi.close()
 
 
+gidx = open("p2idx.gi",'wb')
+tidx = open("p2idx.tx",'wb')
+aidx = open("p2idx.ac",'wb')
 
 
-
-
-searcher = gi_idx.searcher()
+cp.dump(gi,gidx,pickle.HIGHEST_PROTOCOL)
+cp.dump(taxa,tidx,pickle.HIGHEST_PROTOCOL)
+cp.dump(acc,aidx,pickle.HIGHEST_PROTOCOL)
 
 
 
-for bln in blastout:
-    if "#" not in bln:
 
-        #cols = bln.split("|")
-        cols = bln.split("\t")
-        qp = QueryParser("content",schema=gi_idx.schema)
-        query = qp.parse("gi")
 
-        with gi_idx.searcher() as s:
-            results = s.search(query)
-            print(results)
-            for each in results:
-                print(each)
+
+
+
+#for bln in blastout:
+#    if "#" not in bln:
+#
+#        #cols = bln.split("|")
+#        cols = bln.split("\t")
+#        qp = QueryParser("content",schema=gi_idx.schema)
+#        query = qp.parse("gi")
+#
+#        with gi_idx.searcher() as s:
+#            results = s.search(query)
+#            print(results)
+#            for each in results:
+#                print(each)
     
 
 
