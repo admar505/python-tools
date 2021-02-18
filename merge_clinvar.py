a2d6a78b4067b7691638169e43d5166671f7d8f6
Diego Martinez <admar505@gmail.com>
2021-02-18 12:05:29 -0500

    last fix needs to be the pathogenic presentation

--- merge_clinvar.py
+++ merge_clinvar.py
@@ -13,10 +13,6 @@ parser.add_argument("--named",help="the date name prefix, for output files.",req
 #parser.add_argument("--combo",help="combination types",action='append',required=False)
 #parser.add_argument("--ABthreshold",help="this is a value at which to trust allele ballance, default is 15",default=0.15)
 #parser.add_argument("--lc",help="If no genotype can be found, then this is the list of novel or low coverage pages")
-#parser.add_argument("--hap",help="vcf-allele-haplotypes output")
-#parser.add_argument("--trans",help="the gene to transcript file for PGX",required=False)
-#parser.add_argument("--rpt",help='the repeats formatted file, reuse argument for multiple searches.\n format is: \"chrom start stop base leader unit wt_version wt_length hgvs_primitive\"',action='append',required=False)
-#parser.add_argument("--add",help="inject a line, usually used to add results from an external tool and add it to the combo search sections",action='append',required=False)
 
 args = parser.parse_args()
 csv.field_size_limit(sys.maxsize)
@@ -41,7 +37,8 @@ varsumfi = csv.DictReader(open(args.varsum,'r'),delimiter='\t')
 
 varsum_out_name = "hg19_clinvar_" + args.named + ".txt"
 varsum_final = open(varsum_out_name,"w")
-
+bigsum_out_name = args.named + "_variant_summary_GRCh37_annovar.txt"
+bigsum_final = open(bigsum_out_name,"w")
 
 
 #---------subroutines-----------------#
@@ -54,27 +51,36 @@ def getCLN(varline):#there is a double key, one column has to be split.
     CLNDSDB = {}
     CLNDSDBID = {}
 
-    for clnln in varlst:
-        
-        array = clnln.split(":")#because clinvar SUCKS, the ID is only the last entry in this record.
-        arrct  = 0
 
-        while arrct < (len(array) - 1):
-        
-            CLNDSDB[array[arrct]] = arrct
-            arrct = arrct + 1
-
-
-        CLNDSDBID[array[-1]] = array[-1]
     
-    returnstring = ",".join(CLNDSDB.keys()) + "\t" + ",".join(CLNDSDBID.keys())
+    #varlst = varbreak.split("|")#It is unclear as to why the newer clinbar uses commas and 
+                                #why it also uses the the "|" I will attempt to use both.
+
+    for varbreak in varlst:
+        for clnln in varbreak.split('|'):
+      
+            array = clnln.split(":")#because clinvar SUCKS, the ID is only the last entry in this record.
+            arrct  = 0
+
+            while arrct < (len(array) - 1):
+        
+                CLNDSDB[array[arrct]] = arrct
+                arrct = arrct + 1
+
+
+            CLNDSDBID[array[-1]] = array[-1]
+            ctmp = ",".join(CLNDSDB.keys()) 
+            clndsdbid = ctmp.translate(str.maketrans({'|':';'}))
+
+
+    returnstring = clndsdbid + "\t" + ",".join(CLNDSDBID.keys())
     
     return(returnstring)
 
 def checkGuidelines(glines):#simply checks if guidelines are dashes, it needs to be dots.
     
     if "-" in str(glines):
-        glines = "."
+        glines = "NA"
 
     return(glines)
 
@@ -104,6 +110,7 @@ for hln in hgmdfi:#HGMD is going in.
     hgmd[key] = hln['hgmd_accession'] + "\t"  + hln['variant_class'] + "\t"  + hln['primary_pubmed']
 
 varsum_final.write("#Chr\tStart\tEnd\tRef\tAlt\tCLNSIG\tCLNDBN\tCLNACC\tCLNDSDB\tCLNDSDBID\tReview\tGuidelines\tVariationID\thgmdID\thgmdVC\thgmdPUBMED\n")
+bigsum_final.write("#Chr\tStart\tEnd\tRef\tAlt\tCLNSIG\tCLNDBN\tCLNACC\tCLNDSDB\tCLNDSDBID\tReview\tGuidelines\trsID\tVariationID\n")
 
 
 """
@@ -130,14 +137,20 @@ for var in varsumfi:#more about the VARSUM file.
     except KeyError as e:
         hgmdinfo = ".\t.\t."
 
-
+    #backtranslate for formats needed.
     changeguidelinetodot = checkGuidelines(var['Guidelines'])
+    
+    RCVaccession = var['RCVaccession'].translate(str.maketrans({'|':';'}))
+    clnDBN = var['RCVaccession'].translate(str.maketrans({'|':';'}))
+
 
     varsum_final.write(var['Chromosome'] +"\t"+ var['Start'] + "\t" + var['Stop'] + "\t" + var['ReferenceAlleleVCF'] + "\t" + var['AlternateAlleleVCF']\
-             + "\t" + var['ClinicalSignificance'] + "\t" + var['PhenotypeList'] + "\t" + var['RCVaccession'] + "\t" + clnDBS + "\t" + var['ReviewStatus']\
+             + "\t" + var['ClinicalSignificance'] + "\t" + clnDBN + "\t" + RCVaccession + "\t" + clnDBS + "\t" + var['ReviewStatus']\
              + "\t" + changeguidelinetodot + "\t" + var['#AlleleID'] + "\t" + hgmdinfo  +  "\n")
 
-
+    bigsum_final.write(var['Chromosome'] +"\t"+ var['Start'] + "\t" + var['Stop'] + "\t" + var['ReferenceAlleleVCF'] + "\t" + var['AlternateAlleleVCF']\
+             + "\t" + var['ClinicalSignificance'] + "\t" + clnDBN + "\t" + RCVaccession + "\t" + clnDBS + "\t" + var['ReviewStatus']\
+             + "\t" + changeguidelinetodot + "\t" + var['RS# (dbSNP)'] + "\t" +  var['#AlleleID']  +  "\n")
 
 ##Now, fill in the items which did not have a match in the clinvar files.
 
